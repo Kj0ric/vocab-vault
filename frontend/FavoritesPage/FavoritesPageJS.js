ecae3ce01b46933871025a22772348fb90f30eaf
icheukalam <i.o.e.cheuk-a-lam@student.vu.nl>
2024-06-13 13:51:55 +0200

    Added favorites page, fixed dropdown menu bug
    I've added a favorites page and changed some links in other pages to access the favorites page
    I've also fixed the bug where the dropdown menus didn't appear when hovering over them after scrolling down on any of the pages

---
+++ frontend/FavoritesPage/FavoritesPageJS.js
@@ -0,0 +1,98 @@
+
+function adjustButtonMargins() {
+    /*
+    This function changes the size of the dropdown buttons and the search bar.
+    It also govens the text dispaying variables in the down right corner.
+
+    This function has no parameters and returns nothing
+    */
+    const buttons = document.querySelectorAll('.dropdown .dropbtn'); //selects all dropdown buttons
+    const dropdown = document.querySelectorAll('.dropdown-content a:hover'); //should select all buttons in the dropdown menus
+    const width = window.innerWidth; //grabs the width of the browser window
+    const height = window.innerHeight; //grabs the height of the browser window
+    const searchBar = document.querySelector('.navbar input[type= text]'); //selects the searchbar
+    
+    let marginValue;
+    let marginValueNumber;
+
+    let searchBarWidth
+    let searchBarWidthNumber
+    
+    // adjusts the margin value based on the width of the browser window
+    marginValueNumber = width/10-45;
+
+    // changes the margin value to 0 when it becomes less than 0
+    if (marginValueNumber < 0) {
+        marginValueNumber = 0
+    }
+
+    // changes the margin value to a string and adds 'px'
+    marginValue = marginValueNumber + 'px'
+
+    // changes the margin value for each dropdown button
+    buttons.forEach(button => {
+        button.style.paddingRight = marginValue;
+        button.style.paddingLeft = marginValue;
+    });
+    
+    // doesn't work, but the idea was that it changes the size of the buttons in the dropdown menu to match the dropdown buttons
+    dropdown.forEach(button => {
+        button.style.paddingRight = marginValue;
+        button.style.paddingLeft = marginValue;
+    });
+
+    // adjusts the search bar width based on the width of the browser window
+    searchBarWidthNumber = width/3    ;
+
+    // changes the search bar width to 66 when it becomes less than 66
+    if (searchBarWidthNumber < 66) {
+        searchBarWidthNumber = 66
+    }
+
+    // changes the search bar width to a string and adds 'px'
+    searchBarWidth = searchBarWidthNumber + 'px'
+
+    // changes the search bar width
+    searchBar.style.width = searchBarWidth;
+
+
+    // displays the width, height and margin values
+    document.getElementById('windowSize').innerText = `Width: ${width}px, Height: ${height}px, MarginValue: ${marginValue}, SearcBarWidth: ${searchBarWidthNumber}, scrollY: ${window.scrollY}`;
+}
+
+function makeNavBarSticky() {
+    /*
+    This function makes the navbar sticky when the user scrolls down, it also makes it not sticky anymore when the user scrolls back up
+    
+    This function has no parameters and returns nothing
+    */
+    var navbar = document.getElementById("navbar"); // this selects the navbar
+
+    var sticky = navbar.offsetTop; //this is a variable that hold the distance between the top of the page and the top of the navbar
+
+    // this makes the navbar sticky when the navbar reaches the top of the screen
+    if (window.scrollY >= sticky) {
+        navbar.classList.add("sticky")
+    
+    }
+    
+    //this makes the navbar not sticky anymore when the user has scrolled back up
+    if (window.scrollY < 8) {
+        navbar.classList.remove("sticky");
+    }
+}
+
+function combinedScrollFunctions() {
+    /*
+    This function combines the makeNavBarSticky and adjustButtonMArgins functions so they can both be activated when scrolling
+    (the adjustButtonMargins fuction needs to be activated to adjust show the current distance scrolled by the user)
+
+    This function has no parameters and returns nothing
+    */
+    makeNavBarSticky();
+    adjustButtonMargins();
+}
+
+window.onresize = adjustButtonMargins; //activates the adjustButtonMargins function when resizing the browser window
+window.onload = adjustButtonMargins; //activates the adjustButtonMargins function when the browser loads
+window.onscroll = combinedScrollFunctions; //activates the combinedScrollFunctions function when scrolling
